985c8c61cff2bbc15d4aeb7b50151238688fef0c
expeditus303 <ricardotonietto@gmail.com>
2023-08-30 14:07:20 -0300

    feat(users): add CreateUserDto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,11 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty, IsString, IsStrongPassword, MinLength } from "class-validator";
+
+export class CreateUserDto {
+    @IsEmail()
+    @IsNotEmpty()
+    email: string
+
+    @IsStrongPassword({minLength: 10, minLowercase: 1, minUppercase: 1, minSymbols: 1, minNumbers: 1})
+    @IsNotEmpty()
+    password: string
+}
