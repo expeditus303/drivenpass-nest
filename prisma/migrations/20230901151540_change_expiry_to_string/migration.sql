00ea00dfbf3a2691269d91478145e04ff6d2b554
expeditus303 <ricardotonietto@gmail.com>
2023-09-01 12:16:27 -0300

    feat(cards): add DTOs

---
+++ prisma/migrations/20230901151540_change_expiry_to_string/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "cards" ALTER COLUMN "expiryMonth" SET DATA TYPE TEXT,
+ALTER COLUMN "expiryYear" SET DATA TYPE TEXT;
--- src/cards/dto/create-card.dto.ts
+++ src/cards/dto/create-card.dto.ts
@@ -1,1 +1,83 @@
-export class CreateCardDto {}
+import { CardType } from '@prisma/client';
+import {
+  IsBoolean,
+  IsCreditCard,
+  IsIn,
+  IsNotEmpty,
+  IsNumber,
+  IsNumberString,
+  IsString,
+  Length,
+  Max,
+  Min,
+} from 'class-validator';
+
+const CURRENT_YEAR = new Date().getFullYear();
+const MONTHS = [
+  '01',
+  '02',
+  '03',
+  '04',
+  '05',
+  '06',
+  '07',
+  '08',
+  '09',
+  '10',
+  '11',
+  '12',
+];
+const CARD_TYPES = ['CREDIT', 'DEBIT', 'CREDIT_DEBIT'];
+
+export class BaseCardDto {
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @IsString()
+  @IsNotEmpty()
+  cardHolder: string;
+
+  @IsString()
+  @IsIn(MONTHS)
+  @IsNotEmpty()
+  expiryMonth: string;
+
+  @IsNumberString({ no_symbols: true })
+  @Min(CURRENT_YEAR, { message: '' })
+  @Length(4, 4)
+  @IsNotEmpty()
+  expiryYear: string;
+
+  @IsBoolean()
+  @IsNotEmpty()
+  isVirtual: boolean;
+
+  @IsString()
+  @IsIn(CARD_TYPES)
+  @IsNotEmpty()
+  cardType: CardType;
+}
+
+export class CreateCardDto extends BaseCardDto {
+  @IsCreditCard()
+  @IsNotEmpty()
+  cardNumber: string;
+
+  @IsNumberString()
+  @Length(3, 3)
+  @IsNotEmpty()
+  CVC: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+}
+
+export class ProcessedCardDto extends BaseCardDto {
+  encryptedCardNumber: string;
+
+  encryptedCVC: string;
+
+  encryptedPassword: string;
+}
